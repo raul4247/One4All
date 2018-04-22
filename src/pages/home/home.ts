266145a9cb1393b6433edb5a5bfc5cabb76afd47
Raul Mansur <raulfmansur@hotmail.com>
2018-04-21 23:59:19 -0300

    Map added to home screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html'
 })
+
 export class HomePage {
-	constructor(public navCtrl: NavController) {/*
+	@ViewChild('map') mapElement: ElementRef;
+	map: any;
+	
+	constructor(public navCtrl: NavController) {
+		
+	}
+	ionViewDidLoad(){
+		this.loadMap();
+	}
+	loadMap(){
 		var uluru = {lat: -25.363, lng: 131.044};
 		var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 4,
@@ -16,6 +28,15 @@ export class HomePage {
 			position: uluru,
 			map: map
 		});
-*/	}
+		let latLng = new google.maps.LatLng(-34.9290, 138.6010);
 
+		let mapOptions = {
+			center: latLng,
+			zoom: 15,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+
+		this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+	}
 }
